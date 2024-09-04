a7d3b8998c80b00b19b764e9bd9c166279def9e7
Yash <ymalegao@ucsc.edu>
2024-09-04 16:48:52 -0700

    first commit

---
+++ build/CMakeFiles/tictactoe.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/Application.cpp" "CMakeFiles/tictactoe.dir/Application.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/Application.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/classes/Bit.cpp" "CMakeFiles/tictactoe.dir/classes/Bit.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/classes/Bit.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/classes/BitHolder.cpp" "CMakeFiles/tictactoe.dir/classes/BitHolder.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/classes/BitHolder.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/classes/Chess.cpp" "CMakeFiles/tictactoe.dir/classes/Chess.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/classes/Chess.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/classes/Game.cpp" "CMakeFiles/tictactoe.dir/classes/Game.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/classes/Game.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/classes/Log.cpp" "CMakeFiles/tictactoe.dir/classes/Log.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/classes/Log.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/classes/Sprite.cpp" "CMakeFiles/tictactoe.dir/classes/Sprite.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/classes/Sprite.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/classes/Square.cpp" "CMakeFiles/tictactoe.dir/classes/Square.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/classes/Square.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/classes/TTC.cpp" "CMakeFiles/tictactoe.dir/classes/TTC.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/classes/TTC.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/imgui/imgui.cpp" "CMakeFiles/tictactoe.dir/imgui/imgui.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/imgui/imgui.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/imgui/imgui_demo.cpp" "CMakeFiles/tictactoe.dir/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/imgui/imgui_demo.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/imgui/imgui_draw.cpp" "CMakeFiles/tictactoe.dir/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/imgui/imgui_draw.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/imgui/imgui_impl_glfw.cpp" "CMakeFiles/tictactoe.dir/imgui/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/imgui/imgui_impl_glfw.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/imgui/imgui_impl_opengl3.cpp" "CMakeFiles/tictactoe.dir/imgui/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/imgui/imgui_impl_opengl3.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/imgui/imgui_tables.cpp" "CMakeFiles/tictactoe.dir/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/imgui/imgui_tables.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/imgui/imgui_widgets.cpp" "CMakeFiles/tictactoe.dir/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/imgui/imgui_widgets.cpp.o.d"
+  "/Users/yashmalegaonkar/Documents/CMPM123/chess2/main_macos.cpp" "CMakeFiles/tictactoe.dir/main_macos.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/main_macos.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tictactoe.dir/cmake_clean.cmake
@@ -0,0 +1,43 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tictactoe.dir/Application.cpp.o"
+  "CMakeFiles/tictactoe.dir/Application.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/classes/Bit.cpp.o"
+  "CMakeFiles/tictactoe.dir/classes/Bit.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/classes/BitHolder.cpp.o"
+  "CMakeFiles/tictactoe.dir/classes/BitHolder.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/classes/Chess.cpp.o"
+  "CMakeFiles/tictactoe.dir/classes/Chess.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/classes/Game.cpp.o"
+  "CMakeFiles/tictactoe.dir/classes/Game.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/classes/Log.cpp.o"
+  "CMakeFiles/tictactoe.dir/classes/Log.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/classes/Sprite.cpp.o"
+  "CMakeFiles/tictactoe.dir/classes/Sprite.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/classes/Square.cpp.o"
+  "CMakeFiles/tictactoe.dir/classes/Square.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/classes/TTC.cpp.o"
+  "CMakeFiles/tictactoe.dir/classes/TTC.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/imgui/imgui.cpp.o"
+  "CMakeFiles/tictactoe.dir/imgui/imgui.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/tictactoe.dir/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/tictactoe.dir/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/imgui/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/tictactoe.dir/imgui/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/imgui/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/tictactoe.dir/imgui/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/tictactoe.dir/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/tictactoe.dir/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/main_macos.cpp.o"
+  "CMakeFiles/tictactoe.dir/main_macos.cpp.o.d"
+  "tictactoe"
+  "tictactoe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tictactoe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
